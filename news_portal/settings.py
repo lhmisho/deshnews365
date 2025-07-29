c376abb99b141c0988cf771c5295b90ccbaaf21d
lokman.hossen <lokman.hossen@upaybd.com.bd>
2025-07-29 20:59:27 +0600

    mysql added

--- news_portal/settings.py
+++ news_portal/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'news_portal.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'deshnews365',
+        'USER': 'adminuser',   # Or 'adminuser'
+        'PASSWORD': 'KHDGDHHEG(&#43553HDHHD',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
