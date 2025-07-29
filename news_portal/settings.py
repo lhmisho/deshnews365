e2450b2abbf1ced44e8cddf57493da9f6a3369ad
lokman.hossen <lokman.hossen@upaybd.com.bd>
2025-07-29 17:40:42 +0600

    domain added to allowed hosts

--- news_portal/settings.py
+++ news_portal/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-your-secret-key-change-this-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['deshnews365.com', '103.177.125.198']
 
 
 # Application definition
